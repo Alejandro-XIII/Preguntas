5eb55cfc068c993bd42ac63e2d2f8ca3e972b42f
jadercaro <96452959+jadercaro@users.noreply.github.com>
2023-02-16 17:34:02 -0500

    modificación de frame y button.place()

--- main.py
+++ main.py
@@ -23,44 +23,57 @@ screen_height = root.winfo_screenheight()
 # Calcular la ubicación para centrarl la ventana
 screen_width = root.winfo_screenwidth()
 screen_height = root.winfo_screenheight()
-window_width = 400
-window_height = 300
+window_width = 600
+window_height = 580
 x_coord = (screen_width / 2) - (window_width / 2)
 y_coord = (screen_height / 2) - (window_height / 2)
 root.geometry("{}x{}+{}+{}".format(window_width, window_height, int(x_coord), int(y_coord)))
 
+frame = tk.Frame(root, width=600, height=580, bg="red")
+frame.pack()
+frame.place(x=0, y=0)
+
+
+
 # Crear un cuadro de texto
-label = tk.Label(root, text="Este es un cuadro de texto")
+label = tk.Label(frame, text="Este es un cuadro de texto")
 label.pack()
+label.place(x=100, y=100)
+
+
 
 # Crear los botones y asignar acciones
 def on_button1_click():
     response = client.submit_request(client_socket, "El botón 1 ha sido presionado")
     print(response,"Botón 1")
 
-button1 = tk.Button(root, text="Botón 1", command=on_button1_click)
+button1 = tk.Button(frame, text="Botón 1", command=on_button1_click)
 button1.pack()
+button1.place(x=250,y=200)
 
 def on_button2_click():
     response = client.submit_request(client_socket, "El botón 2 ha sido presionado")
     print(response,"Botón 2")
 
-button2 = tk.Button(root, text="Botón 2", command=on_button2_click)
+button2 = tk.Button(frame, text="Botón 2", command=on_button2_click)
 button2.pack()
+button2.place(x=250,y=250)
 
 def on_button3_click():
     response = client.submit_request(client_socket, "El botón 3 ha sido presionado")
     print(response,"Botón 3")
 
-button3 = tk.Button(root, text="Botón 3", command=on_button3_click)
+button3 = tk.Button(frame, text="Botón 3", command=on_button3_click)
 button3.pack()
+button3.place(x=250,y=300)
 
 def on_button4_click():
     response = client.submit_request(client_socket, "El botón 4 ha sido presionado")
     print(response,"Botón 4")
 
-button4 = tk.Button(root, text="Botón 4", command=on_button4_click)
+button4 = tk.Button(frame, text="Botón 4", command=on_button4_click)
 button4.pack()
+button4.place(x=250,y=350)
 
 # Ejecutar el bucle de eventos de tkinter
 root.mainloop()
